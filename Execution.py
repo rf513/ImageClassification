5ed10b537df68db5a7dce5211b41292e120a119d
Reiko Fujiyoshi <rf513@cam.ac.uk>
2024-08-04 14:59:46 +0100

    execution file made

---
+++ Execution.py
@@ -0,0 +1,8 @@
+from DefinedFunctions import *
+
+# link to the directories, make sure it ends with a backslash '/'
+train_path = "C:/Users/RFujiyoshi/Desktop/Nut type/"
+test_path = "C:/Users/RFujiyoshi/Desktop/Nut type test/"
+model_name = "reiko_screw_model_2.keras"
+
+execution(train_path, test_path, model_name)
